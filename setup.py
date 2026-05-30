9e19402f3e832fddc3a1a2972b6f55235db6c39a
x <jordan@upset.bot>
2026-05-30 03:45:16 -0400

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="discord_vr",
+    version="1.0.0",
+    packages=find_packages(),
+    include_package_data=True,
+    license="MIT License",
+    description="Make your discord.py bots appear as if they are on vr.",
+    keywords="discord.py, discord vr, discord vr status, discord bot vr status, discord vr bot status",
+    url="https://github.com/xx347x/Discord-VR",
+    author="xx347x",
+    author_email="inquire@upset.bot",
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+    ]
+)
