e503c97cebd28bb7b09821b605ca17891d70e42f
x <jordan@upset.bot>
2026-05-30 04:17:46 -0400

    F

--- setup.py
+++ setup.py
@@ -1,26 +1,25 @@
 from setuptools import find_packages, setup
 
 setup(
-    name="discord_vr",
+    name="discord-vr",
     version="1.0.0",
     packages=find_packages(),
     include_package_data=True,
-    license="MIT License",
-    description="Make your discord.py bots appear as if they are on vr.",
-    keywords="discord.py, discord vr, discord vr status, discord bot vr status, discord vr bot status",
+    license="MIT",
+    description="Make your discord.py bots appear as if they are in VR.",
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    keywords="discord vr discord.py discord bot status vr presence",
     url="https://github.com/xx347x/Discord-VR",
     author="xx347x",
     author_email="inquire@upset.bot",
+    python_requires=">=3.8",
+    install_requires=[
+        "discord.py>=2.0"
+    ],
     classifiers=[
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 2",
-        "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.5",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: 3.10",
-    ]
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
 )
